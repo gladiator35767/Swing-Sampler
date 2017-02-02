6ebe02edf759d14882f2c2aad4db498832ac5be5
anthony.battista <anthony.battista@MU-C1-15.mullen.pvt.k12.co.us>
2017-02-02 14:21:21 -0700

    Complete Graphics Challenge 1

--- Swing Sampler/src/MyCanvas.java
+++ Swing Sampler/src/MyCanvas.java
@@ -11,7 +11,7 @@ public class MyCanvas extends Canvas
     		{
 	        MyCanvas canvas = new MyCanvas();
 	        JFrame frame = new JFrame();
-	        frame.setSize(500, 400);
+	        frame.setSize(1000, 1000);
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        frame.getContentPane().add(canvas).setBackground(Color.white);
 	        frame.setLocationRelativeTo(null); //Sets JPanel to center of screen 
@@ -21,26 +21,54 @@ public class MyCanvas extends Canvas
  
     	public void paint(Graphics graphics) 
     		{
-    		
-	        graphics.setColor(Color.red);
-	        graphics.fillOval(140, 130, 100, 100);
-	        
-	        graphics.setColor(Color.blue);
-	        graphics.fillRect(10, 10, 200, 100);
+    			        
+            graphics.setColor(Color.black);
+	        graphics.drawLine(500, 0, 0, 500);
 	        
 	        graphics.setColor(Color.black);
-	        graphics.drawLine(40, 30, 330, 380);
+	        graphics.drawLine(0, 0, 500, 500);
 	        
-	        for (int i = 0; i < 350; i++)
-	        	{
-		        graphics.setColor(Color.green);
-		        graphics.fillRect(300, i, 50, 50);
-		        
-		        delay();
-		        
-		        graphics.setColor(Color.white);
-		        graphics.fillRect(300, i, 50, 50);
-	        	}
+	        graphics.setColor(Color.black);
+	        graphics.fillRect(0, 0, 800, 800);
+ 		
+    		graphics.setColor(Color.red);
+    		graphics.drawRect(10, 10, 950, 950);
+    			
+    		graphics.setColor(Color.blue);	
+    		graphics.drawRect(700, 700, 100, 100);
+    		
+    		graphics.setColor(Color.yellow);
+    		graphics.fillRect(701, 701, 99, 99);
+    			
+    		graphics.setColor(Color.yellow);
+    		graphics.fillOval(1, 2, 800, 800);
+    		
+    		graphics.setColor(Color.black);
+    		graphics.fillRect(150, 151, 200, 200);
+    		
+    		graphics.setColor(Color.black);
+    		graphics.fillRect(450, 151, 200, 200);
+    		
+    		graphics.setColor(Color.white);
+    		graphics.fillOval(200, 450, 400, 100);
+    		
+    		
+    		
+    		
+    			
+    		
+    			
+	        
+//	        for (int i = 0; i < 350; i++)
+//	        	{
+//		        graphics.setColor(Color.green);
+//		        graphics.fillRect(300, i, 50, 50);
+//		        
+//		        delay();
+//		        
+//		        graphics.setColor(Color.white);
+//		        graphics.fillRect(300, i, 50, 50);
+//	        	}
     		}
     	
     		public void delay()
