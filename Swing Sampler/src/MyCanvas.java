9b80e53bcd9022a57f36f4142d20500d29edbe48
Anthony.battista <Anthony.battista@MU-C1-15.mullen.pvt.k12.co.us>
2017-02-16 14:35:58 -0700

    nothing important

--- Swing Sampler/src/MyCanvas.java
+++ Swing Sampler/src/MyCanvas.java
@@ -23,53 +23,53 @@ public class MyCanvas extends Canvas
     	public void paint(Graphics graphics) 
     		{
     			        
-          graphics.setColor(Color.black);
-	        graphics.drawLine(500, 0, 0, 500);
-	        
-	        graphics.setColor(Color.black);
-	        graphics.drawLine(0, 0, 500, 500);
-	        
-	        graphics.setColor(Color.black);
-	        graphics.fillRect(0, 0, 800, 800);
- 		
-    		graphics.setColor(Color.red);
-    		graphics.drawRect(10, 10, 950, 950);
+//          graphics.setColor(Color.black);
+//	        graphics.drawLine(500, 0, 0, 500);
+//	        
+//	        graphics.setColor(Color.black);
+//	        graphics.drawLine(0, 0, 500, 500);
+//	        
+//	        graphics.setColor(Color.black);
+//	        graphics.fillRect(0, 0, 800, 800);
+// 		
+//    		graphics.setColor(Color.red);
+//    		graphics.drawRect(10, 10, 950, 950);
+//    			
+//    		graphics.setColor(Color.blue);	
+//    		graphics.drawRect(700, 700, 100, 100);
+//    		
+//    		graphics.setColor(Color.yellow);
+//    		graphics.fillRect(701, 701, 99, 99);
+//    			
+//    		graphics.setColor(Color.yellow);
+//    		graphics.fillOval(1, 2, 800, 800);
+//    		
+//    		graphics.setColor(Color.black);
+//    		graphics.fillRect(150, 151, 200, 200);
+//    		
+//    		graphics.setColor(Color.black);
+//    		graphics.fillRect(450, 151, 200, 200);
+//    		
+//    		graphics.setColor(Color.white);
+//    		graphics.fillOval(200, 450, 400, 100);
+//    		
+//    		}
+    		
+    		
+    		
     			
-    		graphics.setColor(Color.blue);	
-    		graphics.drawRect(700, 700, 100, 100);
-    		
-    		graphics.setColor(Color.yellow);
-    		graphics.fillRect(701, 701, 99, 99);
-    			
-    		graphics.setColor(Color.yellow);
-    		graphics.fillOval(1, 2, 800, 800);
-    		
-    		graphics.setColor(Color.black);
-    		graphics.fillRect(150, 151, 200, 200);
-    		
-    		graphics.setColor(Color.black);
-    		graphics.fillRect(450, 151, 200, 200);
-    		
-    		graphics.setColor(Color.white);
-    		graphics.fillOval(200, 450, 400, 100);
-    		
+	        
+	        for (int i = 0; i < 350; i++)
+	        	{
+		        graphics.setColor(Color.green);
+		        graphics.fillRect(300, i, 50, 50);
+		        
+		        delay();
+		        
+		        graphics.setColor(Color.white);
+		        graphics.fillRect(300, i, 50, 50);
+	        	}
     		}
-    		
-    		
-    		
-    			
-	        
-//	        for (int i = 0; i < 350; i++)
-//	        	{
-//		        graphics.setColor(Color.green);
-//		        graphics.fillRect(300, i, 50, 50);
-//		        
-//		        delay();
-//		        
-//		        graphics.setColor(Color.white);
-//		        graphics.fillRect(300, i, 50, 50);
-//	        	}
-    		
     	
     		public void delay()
     			{
